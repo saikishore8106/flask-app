188f4a6b874985f3c4a3c82008528b012d9044ce
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-08-04 17:36:53 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,15 @@
 
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, Jenkins Docker!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
